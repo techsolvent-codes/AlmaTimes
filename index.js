7e2d2f2c63c51bbb180525c93f717a604da5e197
techsolvent-codes <reports.techsolvent@gmail.com>
2026-08-02 03:50:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000;
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:5173", "http://localhost:4173", "http://localhost:8081", "https://alma-times.netlify.app"],
+  origin: ["http://localhost:3000", "http://localhost:5173", "http://localhost:4173", "http://localhost:8081", "https://alma-times.netlify.app", "https://peachpuff-quail-389638.hostingersite.com/"],
   credentials: true,
 }));
 app.use(express.json());
